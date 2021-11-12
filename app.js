c856d499db12ea37ccb6b9114ff3afdd528d6942
DamO1103 <damianjodriozola@gmail.com>
2021-11-11 22:59:42 -0300

    procfile creado

--- app.js
+++ app.js
@@ -6,9 +6,8 @@ const publicPath = path.resolve(__dirname, "./public");
 
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Servidor corriendo en el puerto 3000");
-    console.log(__dirname);
 })
 
 app.get("/", (req, res) => {
